bfb2d56990e25924e464433a35fb7c3503816aab
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-17 21:30:43 +0100

    logo was added

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -21,18 +21,18 @@ const Nav = () => {
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
       <header className="relative h-16 px-8 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+        <nav
+          className="txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="block small:hidden">
-              <Hamburger setOpen={toggle} />
+              <Hamburger setOpen={toggle}/>
             </div>
             <div className="hidden small:block h-full">
-              <SideMenu searchModalOpen={searchModalOpen} />
+              <SideMenu searchModalOpen={searchModalOpen}/>
             </div>
           </div>
-
-          <div className="flex items-center h-full">
-            <img src="logo.svg" alt="logo"/>
+          <div className="flex items-center h-screen md:h-1/2" style={{ width: "30%", height: "auto" }}>
+            <img className="w-full h-auto" src="logo.svg" alt="Logo"/>
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
@@ -54,13 +54,13 @@ const Nav = () => {
                 Аккаунт
               </Link>
             </div>
-            <CartDropdown />
+            <CartDropdown/>
           </div>
         </nav>
-        <MobileMenu />
+        <MobileMenu/>
       </header>
     </div>
-  );
+  )
 }
 
 export default Nav
