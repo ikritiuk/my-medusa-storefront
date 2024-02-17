e234e29a7dcf6b7db3f5b6f9abc306dbe029d53b
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-17 22:36:21 +0100

    hero banner

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,34 +1,21 @@
-import { Button, Heading } from "@medusajs/ui"
-import InteractiveLink from "@modules/common/components/interactive-link"
-import { Github } from "@medusajs/icons"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+    <div className="relative bg-ui-bg-subtle">
+      <div className="h-[75vh] w-full border-b border-ui-border-base">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center p-6 gap-6">
+          <div className="flex items-center">
+            <img src="collection-cover.jpg" alt="Logo"/>
+          </div>
+          <div className="flex flex-wrap justify-between gap-2">
+            <a href="/store" className="hover:no-underline">
+              <button
+                className="border-ui-border-base bg-ui-bg-subtle border text-small-regular h-10 rounded-rounded p-2 flex-1 hover:shadow-elevation-card-rest transition-shadow ease-in-out duration-150">
+                Магазин
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
