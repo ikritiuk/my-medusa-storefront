ee63e34b2e40b7912388e931c7e9be6a9c1f198c
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-19 15:11:03 +0100

    hero refactoring

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,10 +1,24 @@
 const Hero = () => {
   return (
     <div className="relative bg-ui-bg-subtle">
-      <div className="h-[50vh] w-full border-b border-ui-border-base">
+      <div className="h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2">
           <div className="flex items-center mb-2">
-            <img src="collection-cover.jpg" alt="Logo" className="m-1" />
+            <img
+              src="hero-rolex.jpeg" // Use the larger image for the first one
+              alt="Logo"
+              className="m-1 w-1/2" // Two times bigger than the others, but not exceeding parent width
+            />
+            <img
+              src="hero-piguet.jpg" // Use the smaller images for the second and third
+              alt="Logo"
+              className="m-1 w-1/4"
+            />
+            <img
+              src="herp-patek.jpg" // Use the smaller images for the second and third
+              alt="Logo"
+              className="m-1 w-1/4"
+            />
           </div>
           <div className="flex flex-wrap justify-between gap-1">
             <a href="/store" className="hover:no-underline">
@@ -21,4 +35,4 @@ const Hero = () => {
   );
 };
 
-export default Hero
+export default Hero;
