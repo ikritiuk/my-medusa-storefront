a9b81e68e43d118b11ca986eec902ba02f303960
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-22 22:41:49 +0100

    anchrs were added to hero images

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,27 +2,28 @@ const Hero = () => {
   return (
     <div className="relative bg-ui-bg-subtle">
       <div className="hidden lg:block h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
+        <div
+          className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
-            <a href="collections/rolex">
+            <a href="collections/rolex"
+               className="m-2 w-full md:w-1/2">
               <img
                 src="hero-rolex.jpg"
                 alt="Logo"
-                className="m-2 w-full md:w-1/2"
               />
             </a>
-            <a href="collections/piguet">
+            <a href="collections/piguet"
+               className="m-2 w-1/4">
               <img
                 src="hero-piguet.jpg"
                 alt="Logo"
-                className="m-2 w-1/4"
               />
             </a>
-            <a href="collections/patek">
+            <a href="collections/patek"
+               className="m-2 w-1/4">
               <img
                 src="hero-patek.jpg"
                 alt="Logo"
-                className="m-2 w-1/4"
               />
             </a>
           </div>
@@ -31,33 +32,33 @@ const Hero = () => {
 
       <div className="sm:md:block lg:hidden w-full border-b border-ui-border-base relative overflow-hidden">
         <div className="flex flex-col items-center mb-2">
-          <a href="collections/rolex">
+          <a href="collections/rolex"
+             className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none h-full">
             <img
               src="hero-rolex.jpg"
               alt="hero-image-1"
-              className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none h-full"
             />
           </a>
           <div className="flex items-center justify-between w-full">
-            <a href="collections/piguet">
+            <a href="collections/piguet"
+               className="m-2 w-1/2 h-full">
               <img
                 src="hero-piguet.jpg"
                 alt="hero-image-2"
-                className="m-2 w-1/2 h-full"
               />
             </a>
-            <a href="collections/patek">
+            <a href="collections/patek"
+               className="m-2 w-1/2 h-full">
               <img
                 src="hero-patek.jpg"
                 alt="hero-image-3"
-                className="m-2 w-1/2 h-full"
               />
             </a>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
