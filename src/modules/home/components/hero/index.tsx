af588eade1e57ba928d5516f5e5ceb724ada9c7f
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-22 22:15:33 +0100

    Revert "anchor link was added to collections"
    This reverts commit b13f8b81

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,7 +2,6 @@ const Hero = () => {
   return (
     <div className="relative bg-ui-bg-subtle">
       <div className="hidden lg:block h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
-
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
             <img
@@ -27,29 +26,22 @@ const Hero = () => {
       <div className="sm:md:block lg:hidden w-full border-b border-ui-border-base relative overflow-hidden">
         <div
           className="flex flex-col items-center mb-2"
-          style={{
-            minHeight: '50vh',
-            '@media (max-width: 640px)': { minHeight: 'auto' },
-          }}
         >
           <img
             src="hero-rolex.jpg"
             alt="hero-image-1"
-            className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none"
-            style={{ height: '100%' }}
+            className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none h-full"
           />
           <div className="flex items-center justify-between w-full">
             <img
               src="hero-piguet.jpg"
               alt="hero-image-2"
-              className="w-1/2"
-              style={{ height: '100%' }}
+              className="w-1/2 h-full"
             />
             <img
               src="hero-patek.jpg"
               alt="hero-image-3"
-              className="m-1 w-1/2"
-              style={{ height: '100%' }}
+              className="m-1 w-1/2 h-full"
             />
           </div>
         </div>
@@ -59,5 +51,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-
