d359bc594d09c8f317663118cc959c0a68109fe5
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-02-22 20:42:45 +0100

    temp hero tablet and mobile

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
     <div className="relative bg-ui-bg-subtle">
-      <div className="sm:md:hidden lg:block h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
+      <div className="hidden lg:block h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
 
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
             <img
               src="hero-rolex.jpg" // Use the larger image for the first one
@@ -35,14 +35,14 @@ const Hero = () => {
           <img
             src="hero-rolex.jpg"
             alt="hero-image-1"
-            className="m-1 w-full mb-2 max-w-md max-w-sm md:w-full md:max-w-none sm:w-full sm:max-w-none"
+            className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none"
             style={{ height: '100%' }}
           />
-          <div className="hidden md:flex sm:flex items-center justify-between w-full">
+          <div className="flex items-center justify-between w-full">
             <img
               src="hero-piguet.jpg"
               alt="hero-image-2"
-              className="m-1 w-1/2"
+              className="w-1/2"
               style={{ height: '100%' }}
             />
             <img
