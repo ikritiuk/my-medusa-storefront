c75de87c916a550e2042e8dd3ac8fa5469ba96cd
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-03-18 12:01:22 +0100

    sitemap and robots were added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,42 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://royal-replica.ru',
+      url: "https://royal-replica.ru",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: 'https://royal-replica.ru/faq',
+      url: "https://royal-replica.ru/faq",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
-    }
+    },
+    {
+      url: "https://royal-replica.ru/faq",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://royal-replica.ru/about-us",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://royal-replica.ru/rolex",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://royal-replica.ru/patek",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ]
 }
