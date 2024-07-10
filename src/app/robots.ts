60f765e76f68851b01d01b5886f6cd7814d60ba2
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-07-10 19:26:33 +0200

    sitemap refactoring

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://royal-replica.ru/sitemap.xml',
+    sitemap: 'https://royal-replica.shop/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,37 +3,43 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://royal-replica.ru",
+      url: "https://royal-replica.shop",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://royal-replica.ru/faq",
+      url: "https://royal-replica.shop/faq",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://royal-replica.ru/faq",
+      url: "https://royal-replica.shop/account",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://royal-replica.ru/about-us",
+      url: "https://royal-replica.shop/about-us",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://royal-replica.ru/rolex",
+      url: "https://royal-replica.shop/collections/rolex",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://royal-replica.ru/patek",
+      url: "https://royal-replica.shop/collections/patek",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://royal-replica.shop/collections/piguet",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
