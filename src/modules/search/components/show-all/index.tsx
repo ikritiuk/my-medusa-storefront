dcdc0cdcbe0725a6fca5ca646176ecd80d74309d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-09-24 12:22:29 +0200

    i18n

--- src/modules/search/components/show-all/index.tsx
+++ src/modules/search/components/show-all/index.tsx
@@ -22,7 +22,7 @@ const ShowAll = ({ close }: { close: () => void }) => {
     <Container className="flex flex-col small:flex-row gap-2 justify-center items-center h-fit py-4 small:py-2">
       <Text>Showing the first 6 results.</Text>
       <InteractiveLink href={`/search/${query}`} onClick={close}>
-        View all
+        Просмотреть все
       </InteractiveLink>
     </Container>
   )
