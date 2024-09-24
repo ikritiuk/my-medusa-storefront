a771c9ba734b13ca676560e4bbc50d0b3066c1f8
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-09-24 12:19:23 +0200

    i18n

--- src/modules/checkout/components/payment/index.tsx
+++ src/modules/checkout/components/payment/index.tsx
@@ -212,7 +212,7 @@ const Payment = () => {
             <div className="flex items-start gap-x-1 w-full">
               <div className="flex flex-col w-1/3">
                 <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                  Payment method
+                  Способ оплаты
                 </Text>
                 <Text className="txt-medium text-ui-fg-subtle">
                   {paymentInfoMap[cart.payment_session.provider_id]?.title ||
@@ -228,7 +228,7 @@ const Payment = () => {
               </div>
               <div className="flex flex-col w-1/3">
                 <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                  Payment details
+                  Детали оплаты
                 </Text>
                 <div className="flex gap-2 txt-medium text-ui-fg-subtle items-center">
                   <Container className="flex items-center h-7 w-fit p-2 bg-ui-button-neutral-hover">
