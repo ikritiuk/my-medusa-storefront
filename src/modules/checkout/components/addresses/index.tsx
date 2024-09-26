894be8a645c52c3a1676da4e6bad7b1113613252
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-09-26 16:31:37 +0200

    should be good

--- src/modules/checkout/components/addresses/index.tsx
+++ src/modules/checkout/components/addresses/index.tsx
@@ -1,224 +1,125 @@
 import { useCheckout } from "@lib/context/checkout-context"
-import { PaymentSession } from "@medusajs/medusa"
-import { Button } from "@medusajs/ui"
-import { OnApproveActions, OnApproveData } from "@paypal/paypal-js"
-import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
-import { useElements, useStripe } from "@stripe/react-stripe-js"
-import { useCart } from "medusa-react"
-import React, { useEffect, useState } from "react"
+import { Button, Heading, Text } from "@medusajs/ui"
+import { CheckCircleSolid } from "@medusajs/icons"
+import Spinner from "@modules/common/icons/spinner"
+import BillingAddress from "../billing_address"
+import ShippingAddress from "../shipping-address"
+import Divider from "@modules/common/components/divider"
 
-type PaymentButtonProps = {
-  paymentSession?: PaymentSession | null
-}
+const Addresses = () => {
+  const {
+    sameAsBilling: { state: checked, toggle: onChange },
+    editAddresses: { state: isOpen, open },
+    editShipping: { close: closeShipping },
+    editPayment: { close: closePayment },
+    setAddresses,
+    handleSubmit,
+    cart,
+  } = useCheckout()
 
-const PaymentButton: React.FC<PaymentButtonProps> = ({ paymentSession }) => {
-  const { cart } = useCart()
-
-  const notReady =
-    !cart ||
-    !cart.shipping_address ||
-    !cart.billing_address ||
-    !cart.email ||
-    cart.shipping_methods.length < 1
-      ? true
-      : false
-
-  switch (paymentSession?.provider_id) {
-    case "stripe":
-      return (
-        <StripePaymentButton session={paymentSession} notReady={notReady} />
-      )
-    case "manual":
-      return <ManualTestPaymentButton notReady={notReady} />
-    case "paypal":
-      return (
-        <PayPalPaymentButton notReady={notReady} session={paymentSession} />
-      )
-    default:
-      return <Button disabled>Select a payment method</Button>
-  }
-}
-
-const StripePaymentButton = ({
-                               session,
-                               notReady,
-                             }: {
-  session: PaymentSession
-  notReady: boolean
-}) => {
-  const [submitting, setSubmitting] = useState(false)
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(
-    undefined
-  )
-
-  const { cart } = useCart()
-  const { onPaymentCompleted } = useCheckout()
-
-  const stripe = useStripe()
-  const elements = useElements()
-  const card = elements?.getElement("cardNumber")
-
-  const disabled = !stripe || !elements ? true : false
-
-  const handlePayment = async () => {
-    setSubmitting(true)
-
-    if (!stripe || !elements || !card || !cart) {
-      setSubmitting(false)
-      return
-    }
-
-    await stripe
-      .confirmCardPayment(session.data.client_secret as string, {
-        payment_method: {
-          card: card,
-          billing_details: {
-            name:
-              cart.billing_address.first_name +
-              " " +
-              cart.billing_address.last_name,
-            address: {
-              city: cart.billing_address.city ?? undefined,
-              country: cart.billing_address.country_code ?? undefined,
-              line1: cart.billing_address.address_1 ?? undefined,
-              line2: cart.billing_address.address_2 ?? undefined,
-              postal_code: cart.billing_address.postal_code ?? undefined,
-              state: cart.billing_address.province ?? undefined,
-            },
-            email: cart.email,
-            phone: cart.billing_address.phone ?? undefined,
-          },
-        },
-      })
-      .then(({ error, paymentIntent }) => {
-        if (error) {
-          const pi = error.payment_intent
-
-          if (
-            (pi && pi.status === "requires_capture") ||
-            (pi && pi.status === "succeeded")
-          ) {
-            onPaymentCompleted()
-          }
-
-          setErrorMessage(error.message)
-          return
-        }
-
-        if (
-          (paymentIntent && paymentIntent.status === "requires_capture") ||
-          paymentIntent.status === "succeeded"
-        ) {
-          return onPaymentCompleted()
-        }
-
-        return
-      })
-      .finally(() => {
-        setSubmitting(false)
-      })
+  const handleEdit = () => {
+    open()
+    closeShipping()
+    closePayment()
   }
 
   return (
-    <>
-      <Button
-        disabled={disabled || notReady}
-        onClick={handlePayment}
-        size="large"
-        isLoading={submitting}
-      >
-        Оформить заказ
-      </Button>
-      {errorMessage && (
-        <div className="text-red-500 text-small-regular mt-2">
-          {errorMessage}
+    <div className="bg-white px-4 small:px-8">
+      <div className="flex flex-row items-center justify-between mb-6">
+        <Heading
+          level="h2"
+          className="flex flex-row text-3xl-regular gap-x-2 items-baseline"
+        >
+          Address
+          Адрес доставки
+          {!isOpen && <CheckCircleSolid />}
+        </Heading>
+        {!isOpen && (
+          <Text>
+            <button onClick={handleEdit} className="text-ui-fg-interactive">
+              Edit
+            </button>
+          </Text>
+        )}
+      </div>
+      {isOpen ? (
+        <div className="pb-8">
+          <ShippingAddress checked={checked} onChange={onChange} />
+
+          {!checked && (
+            <div>
+              <Heading
+                level="h2"
+                className="text-3xl-regular gap-x-4 pb-6 pt-8"
+              >
+                Billing address
+              </Heading>
+
+              <BillingAddress />
+            </div>
+          )}
+
+          <Button
+            size="large"
+            className="mt-6"
+            onClick={handleSubmit(setAddresses)}
+          >
+            Continue to delivery
+            Продолжить
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <div className="text-small-regular">
+            {cart && cart.shipping_address ? (
+              <div className="flex items-start gap-x-8">
+                <div className="flex items-start gap-x-1 w-full">
+                  <div className="flex flex-col w-1/3">
+                    <Text className="txt-medium-plus text-ui-fg-base mb-1">
+                      Shipping Address
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.shipping_address.first_name}{" "}
+                      {cart.shipping_address.last_name}
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.shipping_address.address_1}{" "}
+                      {cart.shipping_address.address_2}
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.shipping_address.postal_code},{" "}
+                      {cart.shipping_address.city}
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.shipping_address.country_code?.toUpperCase()}
+                    </Text>
+                  </div>
+
+                  <div className="flex flex-col w-1/3 ">
+                    <Text className="txt-medium-plus text-ui-fg-base mb-1">
+                      Contact
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.shipping_address.phone}
+                    </Text>
+                    <Text className="txt-medium text-ui-fg-subtle">
+                      {cart.email}
+                    </Text>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="">
+                <Spinner />
+              </div>
+            )}
+          </div>
         </div>
       )}
-    </>
+      <Divider className="mt-8" />
+    </div>
   )
 }
 
-const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ""
+export default Addresses
 
-const PayPalPaymentButton = ({
-                               session,
-                               notReady,
-                             }: {
-  session: PaymentSession
-  notReady: boolean
-}) => {
-  const [submitting, setSubmitting] = useState(false)
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(
-    undefined
-  )
-
-  const { cart } = useCart()
-  const { onPaymentCompleted } = useCheckout()
-
-  const handlePayment = async (
-    _data: OnApproveData,
-    actions: OnApproveActions
-  ) => {
-    actions?.order
-      ?.authorize()
-      .then((authorization) => {
-        if (authorization.status !== "COMPLETED") {
-          setErrorMessage(`An error occurred, status: ${authorization.status}`)
-          return
-        }
-        onPaymentCompleted()
-      })
-      .catch(() => {
-        setErrorMessage(`An unknown error occurred, please try again.`)
-      })
-      .finally(() => {
-        setSubmitting(false)
-      })
-  }
-  return (
-    <PayPalScriptProvider
-      options={{
-        "client-id": PAYPAL_CLIENT_ID,
-        currency: cart?.region.currency_code.toUpperCase(),
-        intent: "authorize",
-      }}
-    >
-      {errorMessage && (
-        <span className="text-rose-500 mt-4">{errorMessage}</span>
-      )}
-      <PayPalButtons
-        style={{ layout: "horizontal" }}
-        createOrder={async () => session.data.id as string}
-        onApprove={handlePayment}
-        disabled={notReady || submitting}
-      />
-    </PayPalScriptProvider>
-  )
-}
-
-const ManualTestPaymentButton = ({ notReady }: { notReady: boolean }) => {
-  const [submitting, setSubmitting] = useState(false)
-
-  const { onPaymentCompleted } = useCheckout()
-
-  const handlePayment = () => {
-    setSubmitting(true)
-
-    onPaymentCompleted()
-
-    setSubmitting(false)
-  }
-
-  return (
-    <Button
-      disabled={notReady}
-      isLoading={submitting}
-      onClick={handlePayment}
-      size="large"
-    >
-      Place order
-    </Button>
-  )
-}
-
-export default PaymentButton
