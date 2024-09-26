a1e1165c2e04876d93fffa9f58f34d712849bce5
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-09-26 12:14:26 +0200

    submit refactoring

--- src/modules/checkout/components/payment-button/index.tsx
+++ src/modules/checkout/components/payment-button/index.tsx
@@ -63,55 +63,55 @@ const StripePaymentButton = ({
     //   return
     // }
 
-    await stripe
-      .confirmCardPayment(session.data.client_secret as string, {
-        payment_method: {
-          card: card,
-          billing_details: {
-            name:
-              cart.billing_address.first_name +
-              " " +
-              cart.billing_address.last_name,
-            address: {
-              city: cart.billing_address.city ?? undefined,
-              country: cart.billing_address.country_code ?? undefined,
-              line1: cart.billing_address.address_1 ?? undefined,
-              line2: cart.billing_address.address_2 ?? undefined,
-              postal_code: cart.billing_address.postal_code ?? undefined,
-              state: cart.billing_address.province ?? undefined,
-            },
-            email: cart.email,
-            phone: cart.billing_address.phone ?? undefined,
-          },
-        },
-      })
-      .then(({ error, paymentIntent }) => {
-        if (error) {
-          const pi = error.payment_intent
-
-          if (
-            (pi && pi.status === "requires_capture") ||
-            (pi && pi.status === "succeeded")
-          ) {
-            onPaymentCompleted()
-          }
-
-          setErrorMessage(error.message)
-          return
-        }
-
-        if (
-          (paymentIntent && paymentIntent.status === "requires_capture") ||
-          paymentIntent.status === "succeeded"
-        ) {
-          return onPaymentCompleted()
-        }
-
-        return
-      })
-      .finally(() => {
-        setSubmitting(false)
-      })
+    // await stripe
+    //   .confirmCardPayment(session.data.client_secret as string, {
+    //     payment_method: {
+    //       card: card,
+    //       billing_details: {
+    //         name:
+    //           cart.billing_address.first_name +
+    //           " " +
+    //           cart.billing_address.last_name,
+    //         address: {
+    //           city: cart.billing_address.city ?? undefined,
+    //           country: cart.billing_address.country_code ?? undefined,
+    //           line1: cart.billing_address.address_1 ?? undefined,
+    //           line2: cart.billing_address.address_2 ?? undefined,
+    //           postal_code: cart.billing_address.postal_code ?? undefined,
+    //           state: cart.billing_address.province ?? undefined,
+    //         },
+    //         email: cart.email,
+    //         phone: cart.billing_address.phone ?? undefined,
+    //       },
+    //     },
+    //   })
+    //   .then(({ error, paymentIntent }) => {
+    //     if (error) {
+    //       const pi = error.payment_intent
+    //
+    //       if (
+    //         (pi && pi.status === "requires_capture") ||
+    //         (pi && pi.status === "succeeded")
+    //       ) {
+    //         onPaymentCompleted()
+    //       }
+    //
+    //       setErrorMessage(error.message)
+    //       return
+    //     }
+    //
+    //     if (
+    //       (paymentIntent && paymentIntent.status === "requires_capture") ||
+    //       paymentIntent.status === "succeeded"
+    //     ) {
+    //       return onPaymentCompleted()
+    //     }
+    //
+    //     return
+    //   })
+    //   .finally(() => {
+    //     setSubmitting(false)
+    //   })
   }
 
   return (
