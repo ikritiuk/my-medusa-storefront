de152884a7f5c4aacfd8ed7e0f7d26a96bc7362e
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-10-04 15:14:29 +0200

    Revert: format currency

--- src/modules/common/utils/utils.tsx
+++ src/modules/common/utils/utils.tsx
@@ -1,8 +1,4 @@
 export function formatCurrency(value: string): string {
-  // Trim any extra whitespace from the input
-  value = value.trim();
-
-  // Update the regex to match the desired format
-  const regex = /(\bRUB\s+)([\d,]+)/;
-  return value.replace(regex, '$2 RUB');
+  const regex = /(\bRUB\s+)([\d,]+)\.\d{2}/;
+  return value.replace(regex, '$2 RUB').trim();
 }
