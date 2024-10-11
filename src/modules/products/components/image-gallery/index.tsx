453a2a24da51148ab237b13f1b06cde966137872
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-10-11 13:48:22 +0200

    Revert: disabled downloading images

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -1,25 +1,12 @@
 import { Image as MedusaImage } from "@medusajs/medusa"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
-import { useEffect } from "react"
 
 type ImageGalleryProps = {
   images: MedusaImage[]
 }
 
 const ImageGallery = ({ images }: ImageGalleryProps) => {
-  useEffect(() => {
-    const disableRightClick = (e: MouseEvent) => {
-      e.preventDefault();
-    };
-
-    document.addEventListener('contextmenu', disableRightClick);
-
-    return () => {
-      document.removeEventListener('contextmenu', disableRightClick);
-    };
-  }, []);
-
   return (
     <div className="flex items-start relative">
       <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
@@ -40,11 +27,6 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
                 style={{
                   objectFit: "cover",
                 }}
-              />
-              {/* Transparent Overlay */}
-              <div
-                className="absolute inset-0 bg-transparent"
-                style={{ pointerEvents: 'none' }} // Allow mouse events to pass through
               />
             </Container>
           )
