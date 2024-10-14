2ff37bebc546efe7d0764f12d6d00bc0e80e3cc1
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-10-14 14:05:10 +0200

    disabled right click

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -1,38 +1,57 @@
-import { Image as MedusaImage } from "@medusajs/medusa";
-import { Container } from "@medusajs/ui";
-import WatermarkedImage from "@modules/common/components/canvas/canvas";
-import React from "react";
+import { Image as MedusaImage } from "@medusajs/medusa"
+import { Container } from "@medusajs/ui"
+import Image from "next/image"
+import { useEffect } from "react"
 
 type ImageGalleryProps = {
-  images: MedusaImage[];
-};
+  images: MedusaImage[]
+}
 
-const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+const ImageGallery = ({ images }: ImageGalleryProps) => {
+  useEffect(() => {
+    const disableRightClick = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener('contextmenu', disableRightClick);
+
+    return () => {
+      document.removeEventListener('contextmenu', disableRightClick);
+    };
+  }, []);
+
   return (
     <div className="flex items-start relative">
       <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
-        {images.map((image, index) => (
-          <Container
-            key={image.id}
-            className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
-            id={image.id}
-          >
-            <WatermarkedImage
-              src={image.url}
-              watermark="ROYAL-REPLICA.SHOP"
-              priority={index <= 2}
-              className="absolute inset-0 rounded-rounded"
-              fill
-              sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
-              style={{
-                objectFit: "cover",
-              }}
-            />
-          </Container>
-        ))}
+        {images.map((image, index) => {
+          return (
+            <Container
+              key={image.id}
+              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
+              id={image.id}
+            >
+              <Image
+                src={image.url}
+                priority={index <= 2 ? true : false}
+                className="absolute inset-0 rounded-rounded"
+                alt={`Product image ${index + 1}`}
+                fill
+                sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+              {/* Transparent Overlay */}
+              <div
+                className="absolute inset-0 bg-transparent"
+                style={{ pointerEvents: 'none' }} // Allow mouse events to pass through
+              />
+            </Container>
+          )
+        })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ImageGallery;
+export default ImageGallery
