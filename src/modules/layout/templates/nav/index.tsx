2ad28607cace363c0f2942520e52693c339cc053
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-10-22 15:37:39 +0200

    tg was added

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -51,7 +51,7 @@ const Nav = () => {
                  className="text-decoration-none">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram Icon"
-                  className="w-16 p-2"/></a>
+                  className="w-12 p-2"/></a>
               <Link href="/faq" className="hover:text-ui-fg-base">
                 Вопросы
               </Link>
