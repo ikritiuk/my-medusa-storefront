3c46ed1cb094d4eaf84617ff871041d3e2a35e0f
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-10-24 14:22:53 +0200

    view all link was made bigger

--- src/modules/search/components/show-all/index.tsx
+++ src/modules/search/components/show-all/index.tsx
@@ -21,11 +21,11 @@ const ShowAll = ({ close }: { close: () => void }) => {
   return (
     <Container className="flex flex-col small:flex-row gap-2 justify-center items-center h-fit py-4 small:py-2">
       <Text>Showing the first 6 results.</Text>
-      <span className="text-2xl">
-        <InteractiveLink href={`/search/${query}`} onClick={close}>
-          Просмотреть все
-        </InteractiveLink>
-      </span>
+      <span className="text-2xl !important">
+  <InteractiveLink href={`/search/${query}`} onClick={close}>
+    Просмотреть все
+  </InteractiveLink>
+</span>
     </Container>
   )
 }
