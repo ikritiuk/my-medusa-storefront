36d719a888626300045bc6ca475ffaca243a2a51
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 15:10:34 +0100

    test metadata

--- src/app/(main)/faq/page.tsx
+++ src/app/(main)/faq/page.tsx
@@ -1,15 +1,9 @@
 import FaqTemplate from "@modules/account/templates/faq-template"
-import { Metadata as BaseMetadata } from "next"
+import { Metadata } from "next"
 
-// Extend the Metadata type
-interface Metadata extends BaseMetadata {
-  "yandex-verification"?: string
-}
-
-export const metadata: { title: string; description: string;  "yandex": boolean} = {
+export const metadata: Metadata = {
   title: "Вопросы",
   description: "FAQ",
-  yandex: true
 }
 
 export default function Faq() {
--- src/app/(main)/store/page.tsx
+++ src/app/(main)/store/page.tsx
@@ -4,6 +4,7 @@ import StoreTemplate from "@modules/store/templates"
 export const metadata: Metadata = {
   title: "Store",
   description: "Explore all of our products.",
+  yandex: true
 }
 
 export default function StorePage() {
