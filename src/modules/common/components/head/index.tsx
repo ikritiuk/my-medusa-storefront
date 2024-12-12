871c81ed7d44b9cd49968dcd9da3d7479638cc40
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 15:15:07 +0100

    test metadata

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -5,7 +5,6 @@ type HeadProps = {
   title?: string
   description?: string | null
   image?: string | null
-  yandex?: boolean | null
 }
 
 const Head: React.FC<HeadProps> = ({ title, description, image, yandex }) => {
@@ -16,7 +15,6 @@ const Head: React.FC<HeadProps> = ({ title, description, image, yandex }) => {
       <meta itemProp="name" content={title} />
       {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />}
-      {yandex && <meta itemProp="yandex-verification" content="8e708c798b48a43d" />}
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   )
