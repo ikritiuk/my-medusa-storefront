3a94d212ab73c47c9006443c32205a3ca3790562
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 17:08:30 +0100

    test metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,18 @@
 import { getCollectionsList } from "@lib/data"
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
-import Meta from "@modules/home/components/meta"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Metadata } from "next"
 import { Suspense } from "react"
 import * as React from "react"
-import Head from "@modules/common/components/head"
 
 export const metadata: Metadata = {
   title: "Luxury24 shop",
   description:
     "Top quality replicas",
+  verification: {
+    yandex: "8e708c798b48a43d",
+  },
 }
 
 export default async function Home() {
@@ -19,8 +20,6 @@ export default async function Home() {
 
   return (
     <>
-      <Head/>
-      <Meta/>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
         <FeaturedProducts collections={collections} />
