46fc6a8b07777bf84e22d86c0e38bd7a2245ea17
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 15:21:00 +0100

    test metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Suspense } from "react"
+import NextHead from "next/head"
 
 import { Metadata as BaseMetadata } from "next"
 
@@ -21,9 +22,9 @@ export default async function Home() {
 
   return (
     <>
-      <Head>
+      <NextHead>
         <meta name="yandex-verification" content="8e708c798b48a43d" />
-      </Head>
+      </NextHead>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
         <FeaturedProducts collections={collections} />
