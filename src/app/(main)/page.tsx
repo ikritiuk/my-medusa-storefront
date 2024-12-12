217a11cbdbd084d8529b144f30ce58489b9394e5
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 11:03:59 +0100

    yandex metricas were added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,8 +7,10 @@ import { Suspense } from "react"
 
 export const metadata: Metadata = {
   title: "Luxury24 shop",
-  description:
-    "Top quality replicas",
+  description: "Top quality replicas",
+  other: {
+    "yandex-verification": "8e708c798b48a43d",
+  },
 }
 
 export default async function Home() {
