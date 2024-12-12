d858604adfa8473ce0bf132efb45f6184e8a461b
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 11:35:41 +0100

    Revert: yandex metricas were added (e5bd4445)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,12 +3,12 @@ import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Metadata } from "next"
-import React, { Suspense } from "react"
-import NextHead from "next/head"
+import { Suspense } from "react"
 
 export const metadata: Metadata = {
   title: "Luxury24 shop",
-  description: "Top quality replicas",
+  description:
+    "Top quality replicas",
 }
 
 export default async function Home() {
@@ -16,10 +16,6 @@ export default async function Home() {
 
   return (
     <>
-      <NextHead>
-        {/* Adding the custom meta tag for Yandex verification */}
-        <meta name="yandex-verification" content="8e708c798b48a43d" />
-      </NextHead>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
         <FeaturedProducts collections={collections} />
