20515c33269c700767ad0d9d277cec2aef81b27f
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 12:33:16 +0100

    faq test metadata

--- src/app/(main)/faq/page.tsx
+++ src/app/(main)/faq/page.tsx
@@ -1,9 +1,15 @@
 import FaqTemplate from "@modules/account/templates/faq-template"
-import { Metadata } from "next"
+import { Metadata as BaseMetadata } from "next"
 
-export const metadata: Metadata = {
+// Extend the Metadata type
+interface Metadata extends BaseMetadata {
+  "yandex-verification"?: string
+}
+
+export const metadata: { description: string; "yandex-verification": string; title: string } = {
   title: "Вопросы",
   description: "FAQ",
+  "yandex-verification": "8e708c798b48a43d", // Add custom meta key
 }
 
 export default function Faq() {
