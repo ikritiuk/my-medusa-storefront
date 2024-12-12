93b02a839b85451d8952b3dc4c6f6c3d33a2d210
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 15:57:32 +0100

    test metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,13 +5,11 @@ import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-hom
 import { Metadata } from "next"
 import { Suspense } from "react"
 import NextHead from "next/head"
-import React from "react"
-
+import * as React from "react"
 
 export const metadata: Metadata = {
   title: "Luxury24 shop",
-  description:
-    "Top quality replicas",
+  description: "Top quality replicas",
 }
 
 export default async function Home() {
@@ -20,9 +18,37 @@ export default async function Home() {
   return (
     <>
       <NextHead>
-        <meta itemProp="yandex-verification" content="8e708c798b48a43d" />
-        <meta name="yandex-verification2" content="8e708c798b48a43d" />
+        {/* Yandex.Metrika counter script */}
+        <script type="text/javascript">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+            ym(99172678, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              ecommerce:"dataLayer"
+            });
+          `}
+        </script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/99172678"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </NextHead>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
