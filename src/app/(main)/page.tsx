e5bd444549234a6e25ade82d209fd135989d7b9e
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-12 11:24:24 +0100

    yandex metricas were added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,12 +3,12 @@ import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Metadata } from "next"
-import { Suspense } from "react"
+import React, { Suspense } from "react"
+import NextHead from "next/head"
 
 export const metadata: Metadata = {
   title: "Luxury24 shop",
-  description:
-    "Top quality replicas",
+  description: "Top quality replicas",
 }
 
 export default async function Home() {
@@ -16,6 +16,10 @@ export default async function Home() {
 
   return (
     <>
+      <NextHead>
+        {/* Adding the custom meta tag for Yandex verification */}
+        <meta name="yandex-verification" content="8e708c798b48a43d" />
+      </NextHead>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
         <FeaturedProducts collections={collections} />
--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -15,7 +15,6 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
       {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />}
       <link rel="icon" href="/favicon.ico" />
-      <meta itemProp="yandex-verification" content="8e708c798b48a43d" />
     </NextHead>
   )
 }
