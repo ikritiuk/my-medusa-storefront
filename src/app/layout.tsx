a26b41ab93d3147c5e9284e4ae55ee785adb57f0
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-12-16 19:44:44 +0100

    metrika counter was added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,52 @@
-import Providers from "@modules/providers"
-import "styles/globals.css"
+import Providers from "@modules/providers";
+import "styles/globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                     children,
+                                   }: {
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" data-mode="light">
-      <body>
-        <Providers>
-          <main className="relative">{children}</main>
-        </Providers>
-      </body>
+    <head>
+      {/* Yandex.Metrika Counter */}
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(99172678, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true,
+                  ecommerce:"dataLayer"
+              });
+            `,
+        }}
+      />
+    </head>
+    <body>
+    <noscript>
+      <div>
+        <img
+          src="https://mc.yandex.ru/watch/99172678"
+          style={{ position: "absolute", left: "-9999px" }}
+          alt=""
+        />
+      </div>
+    </noscript>
+    <Providers>
+      <main className="relative">{children}</main>
+    </Providers>
+    </body>
     </html>
-  )
+  );
 }
