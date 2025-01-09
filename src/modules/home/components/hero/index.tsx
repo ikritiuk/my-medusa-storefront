cae5e4469caa35a1563265b5e73ca2a986ae21ab
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-10 00:39:26 +0300

    overwiew

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <Link href="/collections/rolex" className="m-2 w-full">
               <div className="relative w-full h-full overflow-hidden">
                 <video
-                  src="https://medusajs-server.fra1.digitaloceanspaces.com/submariner-hero.mp4"
+                  src="https://medusajs-server.fra1.digitaloceanspaces.com/panda-hero.mp4"
                   className="w-full h-full object-cover translate-y-[-10%]"
                   autoPlay
                   loop
@@ -28,13 +28,13 @@ const Hero = () => {
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
             <Link href="/collections/rolex" className="m-2 w-full md:w-1/2">
-              <img src="hero-rolex.jpg" alt="Logo" />
+              <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-rolex.jpg" alt="Logo" />
             </Link>
             <Link href="/collections/piguet" className="m-2 w-1/4">
-              <img src="hero-piguet.png" alt="Logo" />
+              <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-piguet.png" alt="Logo" />
             </Link>
             <Link href="/collections/patek" className="m-2 w-1/4">
-              <img src="hero-patek.png" alt="Logo" />
+              <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-patek.png" alt="Logo" />
             </Link>
           </div>
         </div>
@@ -66,21 +66,21 @@ const Hero = () => {
         </div>
         <div className="flex flex-col items-center mb-2 gap-4"> {/* Added gap-4 for spacing */}
           <Link href="/collections/rolex" className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none h-full">
-            <img src="hero-rolex.jpg" alt="hero-image-1" />
+            <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-rolex.jpg" alt="hero-image-1" />
           </Link>
           <div className="flex items-center justify-between w-full gap-4"> {/* Added gap-4 */}
             <Link href="/collections/piguet" className="w-1/2 h-full">
-              <img src="hero-piguet.png" alt="hero-image-2" />
+              <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-piguet.png" alt="hero-image-2" />
             </Link>
             <Link href="/collections/patek" className="w-1/2 h-full">
-              <img src="hero-patek.png" alt="hero-image-3" />
+              <img src="https://medusajs-server.fra1.digitaloceanspaces.com/hero-patek.png" alt="hero-image-3" />
             </Link>
           </div>
         </div>
         {/* Full-Width Image for Mobile */}
         <div className="w-full">
           <img
-            src="row-rolex.jpg"
+            src="https://medusajs-server.fra1.digitaloceanspaces.com/row-rolex.jpg"
             alt="Full Width Image"
             className="w-full h-auto mt-4 object-cover"
           />
