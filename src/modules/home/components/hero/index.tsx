2436a41639fb43aafa0a249f32e905b1c7e7ac84
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-09 23:52:12 +0300

    overwiew

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,4 +1,11 @@
 import Link from "next/link";
+import React from "react"
+
+const imageTextRow0 = [
+  "Часы Rolex изготавливаются из высококачественных металлов, таких как нержавеющая сталь 904L, отличающаяся высоким содержанием никеля и хрома. Эта стальная сплав имеет повышенную устойчивость к коррозии и немного тяжелее других видов стали.",
+  "Мастера Rolex с особым вниманием создают свои эксклюзивные хронометры. Их внимание к мельчайшим деталям делает часы идеальными. Наши копии практически невозможно отличить от оригинала без микроскопа.",
+  "Полное соответствие по размеру и функционалу. Обработка краев корпуса, ход безельного кольца, зазор между звеньями браслета. Полуавтоматическая обработка заготовок гарантирует точные размеры и соответствующую тактильность."]
+
 
 const Hero = () => {
   return (
@@ -68,6 +75,49 @@ const Hero = () => {
               <img src="hero-patek.png" alt="hero-image-3" />
             </Link>
           </div>
+        </div>
+      </div>
+      <div className="content-container">
+        <div className="w-full max-w-8xl px-6 py-12 bg-white rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold mb-4">О нас</h1>
+          <p className="text-lg mb-6">
+            В нашем Telegram-канале вы найдете огромное количество отзывов от наших счастливых клиентов.
+            Мы специализируемся на элитных брендах, от архивных моделей до новейших. В нашем магазине вы найдете
+            только самый полный ассортимент.
+            Отличное качество и надежность гарантированы.
+          </p>
+
+
+          <div className="flex flex-col md:flex-row md:flex-wrap md:-mx-2 lg:-mx-4">
+            {[
+              {
+                src: "https://medusajs-server.fra1.digitaloceanspaces.com/static/1-im-detail.jpg",
+                alt: "Photo 1",
+                title: "Лучшие материалы",
+              },
+              {
+                src: "https://medusajs-server.fra1.digitaloceanspaces.com/static/2-im-detail.jpg",
+                alt: "Photo 2",
+                title: "Внимание к мелочам",
+              },
+              {
+                src: "https://medusajs-server.fra1.digitaloceanspaces.com/static/3-im-detail.jpg",
+                alt: "Photo 3",
+                title: "Премиум качество",
+              },
+            ].map((photo, index, array) => (
+              <div key={index} className={`w-full md:w-1/3 lg:w-1/3 md:px-2 lg:px-4 mb-8 `}>
+                <h3 className="text-lg mt-2 font-bold mb-6">{photo.title}</h3>
+                <img
+                  src={photo.src}
+                  alt={photo.alt}
+                  className="w-full h-auto rounded-lg mb-4"
+                />
+                <p className="text-lg">{imageTextRow0[index]}</p>
+              </div>
+            ))}
+          </div>
+
         </div>
       </div>
     </div>
