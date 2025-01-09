a9f9ec0b0b1809e2dce7f8a6b889491a1226213d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-09 18:50:27 +0300

    videos together with photos

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link"
 
 const Hero = () => {
   return (
     <div className="relative bg-ui-bg-subtle">
       {/* Large screen (lg and up) */}
       <div className="hidden lg:block h-screen w-full border-b border-ui-border-base relative overflow-hidden">
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
+        <div
+          className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
             <Link href="/collections/rolex" className="m-2 w-full">
               <div className="relative w-full h-full overflow-hidden">
@@ -19,6 +20,15 @@ const Hero = () => {
                   preload="auto"
                 />
               </div>
+            </Link>
+            <Link href="/collections/rolex" className="m-2 w-full md:w-1/2">
+              <img src="hero-rolex.jpg" alt="Logo"/>
+            </Link>
+            <Link href="/collections/piguet" className="m-2 w-1/4">
+              <img src="hero-piguet.png" alt="Logo"/>
+            </Link>
+            <Link href="/collections/patek" className="m-2 w-1/4">
+              <img src="hero-patek.png" alt="Logo"/>
             </Link>
           </div>
         </div>
@@ -38,10 +48,21 @@ const Hero = () => {
               preload="auto"
             />
           </Link>
+          <Link href="/collections/rolex" className="w-full sm:w-full md:w-full sm:max-w-none md:max-w-none h-full">
+            <img src="hero-rolex.jpg" alt="hero-image-1"/>
+          </Link>
+          <div className="flex items-center justify-between w-full">
+            <Link href="/collections/piguet" className="m-2 w-1/2 h-full">
+              <img src="hero-piguet.png" alt="hero-image-2"/>
+            </Link>
+            <Link href="/collections/patek" className="m-2 w-1/2 h-full">
+              <img src="hero-patek.png" alt="hero-image-3"/>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
