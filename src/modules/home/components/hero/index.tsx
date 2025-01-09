4e085d1afb5b7bdfda3f1a539d87618c25931d17
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-09 23:12:02 +0300

    Revert: button was added

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -33,14 +33,26 @@ const Hero = () => {
       <div className="hidden lg:block h-screen w-full border-b border-ui-border-base relative overflow-hidden">
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
-            <Link href="/collections/rolex" className="m-2 w-full md:w-1/2">
-              <img src="hero-rolex.webp" alt="Logo" />
+            <Link href="/collections/rolex" className="m-2 w-1/4">
+              <img
+                src="hero-rolex.png"
+                alt="Logo"
+                className="object-contain w-full h-auto"
+              />
             </Link>
             <Link href="/collections/piguet" className="m-2 w-1/4">
-              <img src="hero-piguet.png" alt="Logo" />
+              <img
+                src="hero-piguet.png"
+                alt="Logo"
+                className="object-contain w-full h-auto"
+              />
             </Link>
             <Link href="/collections/patek" className="m-2 w-1/4">
-              <img src="hero-patek.png" alt="Logo" />
+              <img
+                src="hero-patek.png"
+                alt="Logo"
+                className="object-contain w-full h-auto"
+              />
             </Link>
           </div>
         </div>
