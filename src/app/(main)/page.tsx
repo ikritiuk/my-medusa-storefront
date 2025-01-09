2b0391910d214d7b915e3d9db137333fb51d0078
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-09 18:45:09 +0300

    videos instead of photos

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,5 @@
-import { getCollectionsList } from "@lib/data"
-import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
-import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Metadata } from "next"
-import { Suspense } from "react"
 import * as React from "react"
 
 export const metadata: Metadata = {
@@ -16,14 +12,9 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
-  const { collections, count } = await getCollectionsList(0, 3)
-
   return (
     <>
       <Hero />
-      <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
-        <FeaturedProducts collections={collections} />
-      </Suspense>
     </>
   )
 }
