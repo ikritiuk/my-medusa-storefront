a9fcb2f3319ecb3e0f9a998c22c9e1afca8db6b1
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-09 18:00:24 +0300

    videos instead of photos

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="hidden lg:block h-[50vh] w-full border-b border-ui-border-base relative overflow-hidden">
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-2 hidden lg:flex">
           <div className="flex items-center mb-2">
-            <Link href="/collections/rolex" className="m-2 w-full md:w-1/3">
+            <Link href="/collections/rolex" className="m-2 w-full">
               <video
                 src="https://medusajs-server.fra1.digitaloceanspaces.com/submariner-hero.mp4"
                 className="w-full"
