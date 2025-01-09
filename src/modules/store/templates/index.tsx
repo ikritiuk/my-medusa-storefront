035c15882cc9d49581d53ae7967fe4f441b178a0
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-10 01:11:22 +0300

    button was added to store

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -3,8 +3,9 @@
 import { StoreGetProductsParams } from "@medusajs/medusa"
 import InfiniteProducts from "@modules/products/components/infinite-products"
 import RefinementList from "@modules/store/components/refinement-list"
-import { useState } from "react"
+import { default as React, useState } from "react"
 import { SortOptions } from "../components/refinement-list/sort-products"
+import FloatingButton from "@modules/home/components/floating-button"
 
 const StoreTemplate = () => {
   const [params, setParams] = useState<StoreGetProductsParams>({})
@@ -19,6 +20,7 @@ const StoreTemplate = () => {
         setSortBy={setSortBy}
       />
       <InfiniteProducts params={params} sortBy={sortBy} />
+      <FloatingButton/>
     </div>
   )
 }
