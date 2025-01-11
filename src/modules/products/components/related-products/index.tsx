29734aab1ab9f52642aeb0ad5e333770de230f65
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-11 19:03:07 +0300

    i18n

--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -57,10 +57,10 @@ const RelatedProducts = ({ product }: RelatedProductsProps) => {
     <div className="product-page-constraint">
       <div className="flex flex-col items-center text-center mb-16">
         <span className="text-base-regular text-gray-600 mb-6">
-          Related products
+          Связанные товары
         </span>
         <p className="text-2xl-regular text-gray-900 max-w-lg">
-          You might also want to check out these products.
+          Вам также могут понравиться эти товары.
         </p>
       </div>
 
