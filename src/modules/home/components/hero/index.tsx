f90a5e75ff34979e3a96ffb07e8b8d3c8b9e230d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-11 14:18:24 +0300

    1 row py removed

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
       </div>
 
       {/* Row of 3 Images */}
-      <div className="hidden lg:block w-full border-b border-ui-border-base py-8">
+      <div className="hidden lg:block w-full border-b border-ui-border-base">
         <div className="flex flex-col justify-center items-center text-center gap-6">
           <div className="grid grid-cols-3 gap-6">
             <div className="group relative cursor-pointer transition-transform hover:scale-105">
