d2a98cd46ebbaac45be026da5ba342d41ba1ccfd
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-12 00:22:25 +0300

    Revert "button has traditional telegram styles"
    This reverts commit bbc0521f

--- src/modules/common/components/button/index.tsx
+++ src/modules/common/components/button/index.tsx
@@ -8,24 +8,21 @@ type ButtonProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button = ({
-                  children,
-                  className,
-                  isLoading = false,
-                  variant = "primary",
-                  ...props
-                }: ButtonProps) => {
+  children,
+  className,
+  isLoading = false,
+  variant = "primary",
+  ...props
+}: ButtonProps) => {
   return (
     <button
       {...props}
       className={clsx(
         "w-full uppercase flex items-center justify-center min-h-[50px] px-5 py-[10px] text-small-regular border transition-colors duration-200 disabled:opacity-50",
         {
-          // Telegram Primary Variant
-          "text-white bg-[#0088cc] border-[#0088cc] hover:bg-white hover:text-[#0088cc] hover:border-[#0088cc] disabled:hover:bg-[#0088cc] disabled:hover:text-white":
+          "text-white bg-gray-900 border-gray-900 hover:bg-white hover:text-gray-900 disabled:hover:bg-gray-900 disabled:hover:text-white":
             variant === "primary",
-
-          // Telegram Secondary Variant
-          "text-[#0088cc] bg-transparent border-[#0088cc] hover:bg-[#f5f8fa]":
+          "text-gray-900 bg-transparent border-gray-920 hover:bg-gray-100":
             variant === "secondary",
         },
         className
