8e3ae8a056b6f81eda8d7fdc4aa8259448c2bcf4
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-20 19:11:39 +0300

    classic telegram button

--- src/modules/home/components/floating-button/index.tsx
+++ src/modules/home/components/floating-button/index.tsx
@@ -7,7 +7,7 @@ const FloatingButton = () => {
     <div className="fixed bottom-8 inset-x-0 flex justify-center z-50">
       <button
         onClick={() => window.open("https://t.me/luxury24info", "_blank")}
-        className="px-4 py-3 md:px-6 md:py-4 bg-black text-white text-center rounded-lg shadow-lg hover:bg-gray-900 transition-transform transform hover:scale-105 md:text-lg"
+        className="px-4 py-3 md:px-6 md:py-4 bg-[#0088cc] text-white text-center rounded-lg shadow-lg hover:bg-[#0077b5] transition-transform transform hover:scale-105 md:text-lg"
         aria-label="Order on Telegram"
       >
         <div className="flex items-center justify-center gap-2">
