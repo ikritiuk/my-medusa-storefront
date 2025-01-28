b2772ef7eda3733b70ae4e8836db302df1e92bb4
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-01-28 18:35:06 +0300

    channel is private, link was changed

--- src/modules/home/components/floating-button/index.tsx
+++ src/modules/home/components/floating-button/index.tsx
@@ -6,7 +6,7 @@ const FloatingButton = () => {
   return (
     <div className="fixed bottom-8 inset-x-0 flex justify-center z-50">
       <button
-        onClick={() => window.open("https://t.me/luxury24info", "_blank")}
+        onClick={() => window.open("https://t.me/+X-jZyQk1Md9kOGVk", "_blank")}
         className="px-4 py-3 md:px-6 md:py-4 bg-[#0088cc] text-white text-center rounded-lg shadow-lg hover:bg-[#0077b5] transition-transform transform hover:scale-105 md:text-lg"
         aria-label="Order on Telegram"
       >
